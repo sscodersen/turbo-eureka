c1204f9cc91a833f08629d2368a14fb933bb7ed0
sscodersen <marketing.businessblueprint@gmail.com>
2023-04-04 14:18:17 -0700

    whoop

--- extension/background.js
+++ extension/background.js
@@ -2,13 +2,13 @@
 chrome.contextMenus.create({
   id: "ask-chatgpt",
   title: "Ask ChatGPT",
-  contexts: ["all"],
+  contexts: ["selection"],
 });
 
 // Listen for when the user clicks on the context menu item
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === "ask-chatgpt") {
-    // Send a message to the content script
-    chrome.tabs.sendMessage(tab.id, { type: "ASK_CHATGPT" });
+    // Send a message to the content script with the selected text
+    chrome.tabs.sendMessage(tab.id, { type: "ASK_CHATGPT", text: info.selectionText });
   }
 });
--- extension/content.js
+++ extension/content.js
@@ -1,5 +1,5 @@
 // Listen for messages from the background script
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
   if (message.type === "ASK_CHATGPT") {
     let originalActiveElement;
     let text;
@@ -32,66 +32,67 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     showLoadingCursor();
 
     // Send the text to the API endpoint
-    fetch("http://localhost:3000", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ message: text }),
-    })
-      .then((response) => response.json())
-      .then(async (data) => {
-        // Use original text element and fallback to current active text element
-        const activeElement =
-          originalActiveElement ||
-          (document.activeElement.isContentEditable && document.activeElement);
-
-        if (activeElement) {
-          if (
-            activeElement.nodeName.toUpperCase() === "TEXTAREA" ||
-            activeElement.nodeName.toUpperCase() === "INPUT"
-          ) {
-            // Insert after selection
-            activeElement.value =
-              activeElement.value.slice(0, activeElement.selectionEnd) +
-              `\n\n${data.reply}` +
-              activeElement.value.slice(
-                activeElement.selectionEnd,
-                activeElement.length
-              );
-          } else {
-            // Special handling for contenteditable
-            const replyNode = document.createTextNode(`\n\n${data.reply}`);
-            const selection = window.getSelection();
-
-            if (selection.rangeCount === 0) {
-              selection.addRange(document.createRange());
-              selection.getRangeAt(0).collapse(activeElement, 1);
-            }
-
-            const range = selection.getRangeAt(0);
-            range.collapse(false);
-
-            // Insert reply
-            range.insertNode(replyNode);
-
-            // Move the cursor to the end
-            selection.collapse(replyNode, replyNode.length);
-          }
-        } else {
-          // Alert reply since no active text area
-          alert(`ChatGPT says: ${data.reply}`);
-        }
-
-        restoreCursor();
-      })
-      .catch((error) => {
-        restoreCursor();
-        alert(
-          "Error. Make sure you're running the server by following the instructions on https://github.com/gragland/chatgpt-chrome-extension. Also make sure you don't have an adblocker preventing requests to localhost:3000."
-        );
-        throw new Error(error);
+    try {
+      const response = await fetch("http://localhost:3000", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ message: text }),
       });
+
+      const data = await response.json();
+
+      // Use original text element and fallback to current active text element
+      const activeElement =
+        originalActiveElement ||
+        (document.activeElement.isContentEditable && document.activeElement);
+
+      if (activeElement) {
+        if (
+          activeElement.nodeName.toUpperCase() === "TEXTAREA" ||
+          activeElement.nodeName.toUpperCase() === "INPUT"
+        ) {
+          // Insert after selection
+          activeElement.value =
+            activeElement.value.slice(0, activeElement.selectionEnd) +
+            `\n\n${data.reply}` +
+            activeElement.value.slice(
+              activeElement.selectionEnd,
+              activeElement.length
+            );
+        } else {
+          // Special handling for contenteditable
+          const replyNode = document.createTextNode(`\n\n${data.reply}`);
+          const selection = window.getSelection();
+
+          if (selection.rangeCount === 0) {
+            selection.addRange(document.createRange());
+            selection.getRangeAt(0).collapse(activeElement, 1);
+          }
+
+          const range = selection.getRangeAt(0);
+          range.collapse(false);
+
+          // Insert reply
+          range.insertNode(replyNode);
+
+          // Move the cursor to the end
+          selection.collapse(replyNode, replyNode.length);
+        }
+      } else {
+        // Alert reply since no active text area
+        alert(`ChatGPT says: ${data.reply}`);
+      }
+
+      restoreCursor();
+    } catch (error) {
+      restoreCursor();
+      alert(
+        "Error. Make sure you're running the server by following the instructions on https://github.com/gragland/chatgpt-chrome-extension. Also make sure you don't have an adblocker preventing requests to localhost:3000."
+      );
+      throw new Error(error);
+    }
   }
 });
 
